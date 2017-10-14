6e03bc7b2873101224ba4dc0e50d039c2d62bb5b
ayako.ushio <usshi1031@gmail.com>
2017-10-14 09:25:30 +0000

    task model

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $task->id }} のタスクの詳細ページ</h1>
+    
+    <p>{{ $task->content }}</p>
+    
+    {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $tasm->id]) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
