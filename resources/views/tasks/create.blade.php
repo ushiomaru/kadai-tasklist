e26b76db8925d922c8e2156dd2c75c34212d2397
ayako.ushio <usshi1031@gmail.com>
2017-10-16 12:40:05 +0000

    modify create

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,15 +6,15 @@
  <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-        
-        <div class="form-group">
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        </div>
+       
+<div class="form-group">
+{!! Form::label('status', 'ステータス:') !!}
+{!! Form::text('status', null, ['class' => 'form-control']) !!}
+ </div>
        
         <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
         </div>
         
         {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
